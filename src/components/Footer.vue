e365f5c9429950d4450b513652914023f81d3081
Antoine Barbier <antoine.barbier01@etu.umontpellier.fr>
2020-03-05 20:10:47 -0500

    Footer informations au centre avec flex

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
     <footer>
-        <p><a href="mailto:mouflaquettes@gmail.com" target="_bank" title="mouflaquettes@gmail.com">mouflaquettes@gmail.com</a></p>
-        <p>© All right reserved - created by <a href="antoinebarbier.fr" title="antoinearbier.fr">Antoine Barbier</a></p>
+        <p>Nous contacter : <a href="mailto:mouflaquettes@gmail.com" target="_bank" title="mouflaquettes@gmail.com">mouflaquettes@gmail.com</a></p>
+        <p>© All right reserved - created by <a href="www.antoinebarbier.fr" target="_bank" title="antoinearbier.fr">Antoine Barbier</a></p>
     </footer>
 </template>
 
@@ -16,9 +16,24 @@ export default {
 footer{
     width:100%;
     height: 80px;
+    display:flex;
+        justify-content: center;
+        flex-direction: column;
     color:white;
-    text-align: left;
-    background:#6cf;
+
+}
+
+p{
+    padding:5px;
+}
+
+a{
+    color:white;
+    text-decoration: none;
+}
+
+a:hover{
+    border-bottom: 1px solid white;
 }
 
 </style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,6 +1,5 @@
 <template>
     <div id="home">
-
     </div>
 </template>
 
@@ -13,7 +12,7 @@ export default {
 <style scoped>
     #home{
         width:100%;
-        min-height: calc(100vh - 80px - 60px - 20px); /* - hauteur du footer et - hauteur du header */
+        min-height: calc(100vh - 80px - 60px - 20px); /* - hauteur du footer et - hauteur du header - 20px(hauteur du padding-top du container)*/
     }
 
 </style>
